40367fdca3811139f6226544f66b7946b305bd8c
Oleksandr Radkevych <aradkevich78@gmail.com>
2024-01-10 06:24:16 +0500

    #1 Test of component bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'class4-component-binding';
+  user = {
+    website: 'https://www.google.com',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg',
+    comments: 'default',
+    commentsnew:'empty',
+  };
+
+  onClick(message:string)
+  {
+    console.log(`You pressed the button ${message}`);
+    window.alert('You pressed the button');
+  }
+  
+  handleComments(event: Event) {
+    this.user.commentsnew = (<HTMLInputElement>event.target).value;
+  }
 }
